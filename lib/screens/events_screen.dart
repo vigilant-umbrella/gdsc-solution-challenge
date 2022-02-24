9b591d426a804da817196aec4623731ffc1804f4
TheNinza <niks.a3198@gmail.com>
2022-02-24 11:24:05 +0530

    added For You section and pull to refresh action, set up providers accordingly

--- lib/screens/events_screen.dart
+++ lib/screens/events_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:gdsc_solution_challenge/providers/event_provider.dart';
 import 'package:gdsc_solution_challenge/providers/theme_provider.dart';
-import 'package:gdsc_solution_challenge/widgets/event_card.dart';
+import 'package:gdsc_solution_challenge/widgets/event_list.dart';
+import 'package:gdsc_solution_challenge/widgets/for_you_event_list.dart';
 import 'package:gdsc_solution_challenge/widgets/new_event.dart';
 import 'package:gdsc_solution_challenge/widgets/glassy_bottom_navbar.dart';
 import 'package:provider/provider.dart';
@@ -29,30 +30,77 @@ class EventsScreen extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Events'),
         ),
-        body: FutureBuilder(
-          future: context.read<Events>().fetchAndSetEvents(),
-          builder: (ctx, dataSnapshot) {
-            if (dataSnapshot.connectionState == ConnectionState.waiting) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-            if (dataSnapshot.error != null) {
-              return const Center(
-                child: Text('An error occurred!'),
-              );
-            }
-            return Consumer<Events>(
-              builder: (ctx, eventData, _) => ListView.builder(
-                itemCount: eventData.events.length,
-                itemBuilder: (ctx, i) {
-                  return EventCard(
-                    event: eventData.events[i],
-                  );
-                },
+
+        // refresh indicator adds swipe to refresh functionality
+        body: LayoutBuilder(
+          builder: (context, constraints) => RefreshIndicator(
+            backgroundColor: Colors.white,
+            onRefresh: () => context.read<Events>().fetchAndSetAllEvents(),
+            child: SingleChildScrollView(
+              physics: const AlwaysScrollableScrollPhysics(),
+              child: ConstrainedBox(
+                constraints: BoxConstraints(
+                  minHeight: constraints.maxHeight,
+                ),
+                child: Column(
+                  children: [
+                    // for you list
+                    SizedBox(
+                      height: constraints.maxHeight * 0.32,
+                      child: Column(
+                        children: const [
+                          Padding(
+                            padding: EdgeInsets.all(10),
+                            child: Align(
+                              alignment: Alignment.topLeft,
+                              child: Text(
+                                'For You',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 160,
+                            width: double.infinity,
+                            child: ForYouEventList(),
+                          ),
+                        ],
+                      ),
+                    ),
+
+                    SizedBox(
+                      height: constraints.maxHeight * 0.68,
+                      child: Column(
+                        children: const [
+                          Padding(
+                            padding: EdgeInsets.all(10),
+                            child: Align(
+                              alignment: Alignment.topLeft,
+                              child: Text(
+                                'All Events',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ),
+
+                          // all events list
+                          Expanded(
+                            child: EventList(),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            );
-          },
+            ),
+          ),
         ),
         bottomNavigationBar: const GlassyCustomBottomNavBar(),
         extendBody: true,
---
+++ lib/widgets/for_you_event_card.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:gdsc_solution_challenge/models/event_model.dart';
+import 'package:glass_kit/glass_kit.dart';
+
+class ForYouEventCard extends StatelessWidget {
+  final Event event;
+
+  const ForYouEventCard({
+    required this.event,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GlassContainer.frostedGlass(
+      height: 160,
+      width: 200,
+      margin: const EdgeInsets.symmetric(horizontal: 5),
+      borderRadius: BorderRadius.circular(20),
+      borderWidth: 2,
+      child: InkWell(
+        onTap: () {},
+        child: Container(
+          margin: const EdgeInsets.only(top: 2),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              ClipRRect(
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(18),
+                  topRight: Radius.circular(18),
+                ),
+                child: Image.network(
+                  event.image,
+                  height: 70,
+                  width: 196,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  children: [
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: FittedBox(
+                        child: Text(
+                          event.eventTitle,
+                          style: Theme.of(context).textTheme.titleMedium,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 5),
+                    Column(
+                      children: [
+                        Row(
+                          children: [
+                            const Icon(
+                              Icons.calendar_today,
+                              size: 15,
+                              color: Colors.white,
+                            ),
+                            const SizedBox(width: 10),
+                            Text(
+                              event.date,
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            const Icon(
+                              Icons.location_on,
+                              size: 15,
+                              color: Colors.white,
+                            ),
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            FittedBox(
+                              child: Text(
+                                event.venue,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
